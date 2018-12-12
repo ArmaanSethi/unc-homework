902030f9dda18da17f70e71bbed8522cc3936784
Armaan Sethi <armaan.sethi@gmail.com>
2018-12-12 16:26:03 -0500

    remove comp410 folder

--- Comp 410/410-A3/src/MinBinHeap_A3/EntryPair_Interface.java
+++
@@ -1,6 +0,0 @@
-package MinBinHeap_A3;
-
-public interface EntryPair_Interface {
-  String getValue();
-  int getPriority();
-}
--- Comp 410/410-A6/src/A6_Dijkstra/RunTests.java
+++
@@ -1,9 +0,0 @@
-package A6_Dijkstra;
-
-import gradingTools.comp410s17.assignment6.testcases.Assignment6Suite;
-
-public class RunTests {
-  public static void main(String[] args){
-    Assignment6Suite.main(args);
-  }
-}
